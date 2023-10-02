a913f6ed5e14cf5b95de7c1de6b059e469095d32
Nate Sobeck <nsobeck910@gmail.com>
2023-10-02 16:44:57 -0400

    wrote FloorPlan, Kitchen and Oven components

---
+++ src/FloorPlan.jsx
@@ -0,0 +1,15 @@
+const FloorPlan = () => {
+  return (
+    <div>
+      <Kitchen />
+      <LivingRoom />
+      <Bedroom />
+      <Bedroom />
+      <Bedroom />
+      <Bath />
+      <Bath />
+    </div>
+  )
+}
+
+export default FloorPlan
---
+++ src/Kitchen.jsx
@@ -0,0 +1,12 @@
+import Sink from "./Sink"
+import Oven from "./Oven"
+
+const Kitchen = () => {
+  return ( 
+    <div>
+      <Oven />
+      <Sink />
+    </div>
+  )
+}
+export default Kitchen
